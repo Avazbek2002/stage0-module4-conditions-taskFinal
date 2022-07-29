865b96a19f8b1fe6e6d224d5c2b528a0ab352b9f
Avazbek Isroilov <64166521+Avazbek2002@users.noreply.github.com>
2022-07-30 02:10:08 +0500

    Final Task

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,61 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        switch (month) {
+            case (1):
+                System.out.println(31);
+                break;
+            case (2):
+                if (isLeapYear(year))
+                    System.out.println (29);
+                else
+                    System.out.println(28);
+                break;
+            case (3):
+                System.out.println(31);
+                break;
+            case (4):
+                System.out.println(30);
+                break;
+            case (5):
+                System.out.println(31);
+                break;
+            case (6):
+                System.out.println(30);
+                break;
+            case (7):
+                System.out.println(31);
+                break;
+            case (8):
+                System.out.println(31);
+                break;
+            case (9):
+                System.out.println(30);
+                break;
+            case (10):
+                System.out.println(31);
+                break;
+            case (11):
+                System.out.println(30);
+                break;
+            case (12):
+                System.out.println(31);
+                break;
+            default:
+                System.out.println("wrong number!");
+                break;
+        }
+    }
 
+    public boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 != 0)
+                return true;
+            else if (year % 400 == 0) {
+                return true;
+            } else
+                return false;
+        } else
+            return false;
     }
 }
